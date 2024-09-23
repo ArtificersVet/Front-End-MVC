0de30b8cbd5e7aca1284ff2d81c0eac727a8a572
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-09-23 11:27:50 -0600

    se consume el endoint con interfaz

--- MVC-NPANTS/Controllers/RolesController.cs
+++ MVC-NPANTS/Controllers/RolesController.cs
@@ -11,26 +11,31 @@ namespace MVC_NPANTS.Controllers
         {
             _httpClient = httpClientFactory.CreateClient("CRMAPI");
         }
-        public async Task<List<roles>> Index()
+        public async Task<IActionResult> Index()
         {
+            List<roles> roles = new List<roles>();
+
             try
             {
                 // URL completa de la API
                 var response = await _httpClient.GetFromJsonAsync<List<roles>>("http://localhost:3000/roles");
 
-                if (response == null)
+                if (response != null)
+                {
+                    roles = response;
+                }
+                else
                 {
                     Console.WriteLine("No se encontraron roles.");
-                    return new List<roles>(); 
                 }
-
-                return response; 
             }
             catch (HttpRequestException ex)
             {
                 Console.WriteLine($"Error al hacer la solicitud: {ex.Message}");
-                return new List<roles>(); 
             }
+
+            // Retornamos la vista con el modelo
+            return View(roles);
         }
     }
 }
