d26315746b19b0625945d07fd9c7c35875e73d3f
oswaldo chilin <oswaldochilin49@gmail.com>
2024-11-26 10:54:34 -0600

    usuario pag

--- MVC-NPANTS/Models/usuario.cs
+++ MVC-NPANTS/Models/usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace MVC_NPANTS.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MVC_NPANTS.Models
 {
     public class usuario
     {
@@ -7,5 +9,15 @@
         public string nombre { get; set; }
         public string email { get; set; }
         public string password { get; set; }
+    }
+
+    public class usuariosPaged
+    {
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        [JsonPropertyName("usuarios")]
+        public List<usuario> usuarios { get; set; } = new List<usuario>();
     }
 }
